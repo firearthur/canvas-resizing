bd14b95f95dd0a727743a25f2335a507b00250c0
fireArthur <thefirearthur@gmail.com>
2018-06-12 18:01:06 -0700

    [Style] expands canvas to the full width and height of the screen

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 window.onload = () => {
-  // alert('I loaded');
-  
+  const canvas = document.querySelector('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  console.log(canvas);
+
 };
