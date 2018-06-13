0fdd27bb4f7c4e316fd48156e51a1ef476bf6b08
fireArthur <thefirearthur@gmail.com>
2018-06-12 18:26:19 -0700

    [Feature] Creates circles using for loop and sets x and y dynamically

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ window.onload = () => {
   const canvas = document.querySelector('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  console.log(canvas);
 
+  const cntxt = canvas.getContext('2d');
+
+  cntxt.fillStyle = 'rgb(255,0,0,0.5)';
+  cntxt.fillRect(100, 100, 100, 100);
+  cntxt.fillStyle = 'rgb(0,0,255,0.5)';
+  cntxt.fillRect(200, 150, 100, 100);
+
+
+  cntxt.beginPath();
+  cntxt.moveTo(50, 300);
+  cntxt.lineTo(300, 100);
+  cntxt.lineTo(400, 300);
+  cntxt.strokeStyle = '#666666';
+  cntxt.stroke();
+
+
+  for (let i = 0; i < 500; i++) {
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    cntxt.beginPath();
+    cntxt.arc(x, y, 30, 0, Math.PI * 2, false);
+    cntxt.strokeStyle = 'blue';
+    cntxt.stroke();
+  }
 };
